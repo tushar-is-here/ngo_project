fbcab5019cfdf0d60f5409a833da30af7d689adf
Tushar Panchal <tpanchal484@gmail.com>
2024-05-20 16:23:48 +0530

    committed by Tushar, added state, district and city data API

--- src/main/java/com/ngo/project/Repository/RegisterUserTblRepository.java
+++ src/main/java/com/ngo/project/Repository/RegisterUserTblRepository.java
@@ -4,6 +4,7 @@ import com.ngo.project.Entity.RegisterUserTbl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface RegisterUserTblRepository extends JpaRepository<RegisterUserTbl, Long> {
@@ -11,4 +12,6 @@ public interface RegisterUserTblRepository extends JpaRepository<RegisterUserTbl
 
     @Query(value="select id from register_user_tbl order by id desc limit 1",nativeQuery = true)
     Long getMaxId();
+    @Query(value="select max(created_on) FROM register_user_tbl",nativeQuery = true)
+    String getCreatedOn();
 }
